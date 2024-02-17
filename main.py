73ec876b76c177436d72a024707b7e0897a89fc4
Vallyah <boulot@blaise.cardonne.eu>
2024-02-17 23:53:23 +0100

    add main function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-path_to_file = "books/frankenstein.txt"
+def main():
+    path_to_file = "books/frankenstein.txt"
 
-with open(path_to_file) as file:
-    file_contents = file.read()
-    print(file_contents)
+    with open(path_to_file) as file:
+        file_contents = file.read()
+        print(file_contents)
+
+main()
