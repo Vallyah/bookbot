889afe23e8bf4742e209e328757b819ec562c466
Vallyah <boulot@blaise.cardonne.eu>
2024-02-18 00:08:33 +0100

    count letters

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def main():
 
     with open(path_to_file) as file:
         file_contents = file.read()
-        print(count_words(file_contents))
+        print(count_letters(file_contents))
 
 def count_words(string):
     words = string.split()
     return len(words)
+
+def count_letters(string):
+    result = {}
+    for letter in string:
+        letter = letter.lower()
+        if letter in result:
+            result[letter] += 1
+        else:
+            result[letter] = 1
+    return result
 
 main()
